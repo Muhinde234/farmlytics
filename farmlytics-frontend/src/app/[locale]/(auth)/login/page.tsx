0aa6c0b1701b955db51de14cc83575885997c3be
IGIRIMPUHWE Dositha <igirimpuhwedosta@gmail.com>
2025-10-02 07:29:15 +0300

    change in page root

--- farmlytics-frontend/src/app/[locale]/(auth)/login/page.tsx
+++ farmlytics-frontend/src/app/[locale]/(auth)/login/page.tsx
@@ -9,7 +9,7 @@ export default async function LoginPage({ params: { lang } }: LoginPageProps) {
   const dict = await getDictionary(lang as any); // Type assertion for simplicity, handle properly
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div className="flex items-center justify-center h-screen ">
       <LoginForm dict={dict.login} lang={lang} />
     </div>
   );
