a40be76e5da6f55d7daee6076ba7a5d34942c703
Muhinde Igirimpuhwe Dositha <igirimpuhwedosta@gmail.com>
2025-10-10 15:28:18 +0300

    Update styled.d.ts

--- FarmlyticsMobile/src/config/styled.d.ts
+++ FarmlyticsMobile/src/config/styled.d.ts
@@ -1,7 +1,7 @@
-// src/config/styled.d.ts
+
 import 'styled-components/native';
-import { Theme } from './theme'; // Import your Theme type from your theme file
+import { Theme } from './theme'; 
 
 declare module 'styled-components/native' {
-  export interface DefaultTheme extends Theme {} // Extend DefaultTheme with your custom Theme
+  export interface DefaultTheme extends Theme {} 
 }
