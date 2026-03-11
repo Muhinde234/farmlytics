820343c72358bc25d3960e7fe13c7a27a39b4b02
IGIRIMPUHWE Dositha <igirimpuhwedosta@gmail.com>
2026-03-11 20:25:12 +0100

    issue fixed

--- farmlytics-frontend/next.config.ts
+++ farmlytics-frontend/next.config.ts
@@ -3,7 +3,15 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   eslint: {
-    ignoreDuringBuilds: true, 
+    ignoreDuringBuilds: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/proxy/:path*',
+        destination: 'https://farmlytics1-1.onrender.com/api/v1/:path*',
+      },
+    ]
   },
 };
 
